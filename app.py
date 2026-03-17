19293f3a02084d47280b85a739ca3de6ccb761de
reenakumari0904 <reenakumari0522@gmail.com>
2026-03-17 11:56:00 +0530

    fixed gitignore

---
+++ app.py
@@ -0,0 +1,91 @@
+import numpy as np
+import pandas as pd
+import yfinance as yf
+from keras.models import load_model
+import streamlit as st
+import matplotlib.pyplot as plt
+
+model = load_model('C:/Users/reena/Desktop/ML_project/Stock Predictions Model.keras')
+
+st.header('Stock Market Predictor')
+
+stock = st.text_input('Enter Stock Symbol', 'GOOG')
+start = '2012-01-01'
+end = '2025-12-31'
+
+data = yf.download(stock, start, end)
+
+st.subheader('Stock Data')
+st.write(data)
+
+# Train-Test Split
+data_train = pd.DataFrame(data.Close[0:int(len(data)*0.80)])
+data_test = pd.DataFrame(data.Close[int(len(data)*0.80): len(data)])
+
+from sklearn.preprocessing import MinMaxScaler
+scaler = MinMaxScaler(feature_range=(0,1))
+
+# Take last 100 days from training
+pas_100_days = data_train.tail(100)
+
+# Combine with test data
+data_test = pd.concat([pas_100_days, data_test], ignore_index=True)
+
+# Scale data
+data_test_scaler = scaler.fit_transform(data_test)
+
+# Create sequences
+st.subheader(' Price vs MA50')
+ma_50_days = data.Close.rolling(50).mean()
+fig1 = plt.figure(figsize=(8,6))
+plt.plot(ma_50_days,'r')
+plt.plot(data.Close, 'g')
+plt.show()
+st.pyplot(fig1)
+
+st.subheader('Price vs MA50 vs MA100')
+ma_100_days = data.Close.rolling(100).mean()
+fig2 = plt.figure(figsize=(8,6))
+plt.plot(ma_50_days,'r')
+plt.plot(ma_100_days,'b')
+plt.plot(data.Close, 'g')
+plt.show()
+st.pyplot(fig2)
+
+st.subheader('Price vs MA100 vs MA200')
+ma_200_days = data.Close.rolling(200).mean()
+fig3 = plt.figure(figsize=(8,6))
+plt.plot(ma_100_days,'r')
+plt.plot(ma_200_days,'b')
+plt.plot(data.Close, 'g')
+plt.show()
+st.pyplot(fig3)
+
+
+
+x = []
+y = []
+
+for i in range(100, data_test_scaler.shape[0]):
+    x.append(data_test_scaler[i-100:i])
+    y.append(data_test_scaler[i])
+
+# Convert to numpy arrays (OUTSIDE loop)
+x = np.array(x)
+y = np.array(y)
+
+predict = model.predict(x)
+scale= 1/scaler.scale_
+
+predict = predict*scale
+y= y*scale
+
+st.subheader('Original Price vs Predicted Price')
+fig4 = plt.figure(figsize=(8,6))
+plt.plot(predict,'r', label='Original Price')
+plt.plot(y,'g' ,label='Predicted Price')
+plt.xlabel('Time')
+plt.ylabel('Price')
+plt.show()
+st.pyplot(fig4)
+
